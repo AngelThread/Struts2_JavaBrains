aba84d721c7b369e8c377571a46960b056475c5c
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-17 10:43:43 +0200

    Struts 2 Application Part 1

---
+++ Struts2Starter/src/com/ugur/yeter/struts/action/TutorialAction.java
@@ -0,0 +1,9 @@
+package com.ugur.yeter.struts.action;
+
+public class TutorialAction {
+
+	public String execute(){
+		System.out.println("Hello from execute method of TutorialAction");
+		return null;
+	}
+}
