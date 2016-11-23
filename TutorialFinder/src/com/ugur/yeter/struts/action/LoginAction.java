80bf093b6f5f3ff7b2d2358d991d146839b1ea7f
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-23 18:02:33 +0200

    Login Action and Best Practices

---
+++ TutorialFinder/src/com/ugur/yeter/struts/action/LoginAction.java
@@ -0,0 +1,31 @@
+package com.ugur.yeter.struts.action;
+
+public class LoginAction {
+	private String userId;
+	private String password;
+
+	public String execute() {
+
+		if (getUserId().equalsIgnoreCase("userId") && getPassword().equalsIgnoreCase("password"))
+			return "success";
+		else
+			return "failure";
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
