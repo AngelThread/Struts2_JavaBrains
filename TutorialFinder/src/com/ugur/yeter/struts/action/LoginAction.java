8e64e76bce568c99b74e9391007be2f1dc057a8c
Uğur Yeter <ytr.ugur@gmail.com>
2016-11-29 15:35:32 +0200

    Features of extending ActionSupport class

--- TutorialFinder/src/com/ugur/yeter/struts/action/LoginAction.java
+++ TutorialFinder/src/com/ugur/yeter/struts/action/LoginAction.java
@@ -1,17 +1,35 @@
 package com.ugur.yeter.struts.action;
 
-import com.opensymphony.xwork2.Action;
+import org.apache.commons.lang.StringUtils;
 
-public class LoginAction implements Action{
+// import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import freemarker.template.utility.StringUtil;
+
+// public class LoginAction implements Action{
+public class LoginAction extends ActionSupport {
+
 	private String userId;
 	private String password;
 
+	// @Override it is for Action interface which we implemented
 	public String execute() {
 
 		if (getUserId().equalsIgnoreCase("userId") && getPassword().equalsIgnoreCase("password"))
 			return SUCCESS;
 		else
 			return INPUT;
+	}
+
+	@Override
+	public void validate() {
+		if (StringUtils.isEmpty(getUserId())) {
+			addFieldError("userId", "User Id cannot be blank");
+		}
+		if (StringUtils.isEmpty(getPassword())) {
+			addFieldError("password", "Password cannot be blank");
+		}
 	}
 
 	public String getUserId() {
